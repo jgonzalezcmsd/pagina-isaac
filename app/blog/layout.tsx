348e7e268dd19d1108cef827b2f8c33dfa0fd4cc
Jose Gonzalez Riquelme <jgonzalez@cmsd.site>
2025-07-29 12:39:28 -0400

    Arreglo declaracion

--- app/blog/layout.tsx
+++ app/blog/layout.tsx
@@ -1,26 +1,22 @@
+import { Footer, Navbar } from "@/components";
+import localFont from "next/font/local";
 
-import { Footer, Navbar } from '@/components';
-import localFont from 'next/font/local';
+const geistSans = localFont({
+  src: "../fonts/GeistVF.woff",
+  variable: "--font-geist-sans",
+  weight: "100 900",
+});
+const geistMono = localFont({
+  src: "../fonts/GeistMonoVF.woff",
+  variable: "--font-geist-mono",
+  weight: "100 900",
+});
 
 export default function BlogLayout({
   children,
 }: {
   children: React.ReactNode;
 }) {
-
-    const geistSans = localFont({
-      src: "../fonts/GeistVF.woff",
-      variable: "--font-geist-sans",
-      weight: "100 900",
-    });
-    const geistMono = localFont({
-      src: "../fonts/GeistMonoVF.woff",
-      variable: "--font-geist-mono",
-      weight: "100 900",
-    });
-  
-
-
   return (
     <>
       <Navbar />
