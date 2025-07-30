dbfa5c09478995eb180cb5682775a3b82d39c3aa
Jose Gonzalez Riquelme <jgonzalez@cmsd.site>
2025-07-30 12:32:42 -0400

    Arreglo certificados

--- server.js
+++ server.js
@@ -8,12 +8,9 @@ const hostname = 'projectbim.cl';
 const port = 443;
 
 const options = {
-  key: fs.readFileSync('/home/project/ssl/keys/df823_b098b_a6094588e439c38a5dc74d9f1325ff8e.key'), // Ruta a tu archivo .key
-  cert: fs.readFileSync('/home/project/ssl/certs/projectbim_cl_df823_b098b_1736135299_3dd40c50fa44d04ca9601b8667058f13.crt'), // Usar el archivo correcto aquí
-  ca: [
-    fs.readFileSync('/home/project/ssl/certs/projectbim_cl_df823_b098b_1736135299_3dd40c50fa44d04ca9601b8667058f13.crt'), // Archivo principal (si corresponde)
-    // Agregar aquí otros certificados intermedios si están disponibles
-  ],
+  key: fs.readFileSync('/etc/letsencrypt/live/projectbim.cl/privkey.pem'), // Clave privada ECDSA
+  cert: fs.readFileSync('/etc/letsencrypt/live/projectbim.cl/fullchain.pem'), // fullchain incluye cert + intermediarios
+  // No necesitas especificar 'ca' aparte si usas fullchain.pem
 };
 
 const app = next({ dev, hostname, port });
